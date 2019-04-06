853137bd33b48d9fd620baa0211212eb6ffcb50d
Monadic Cat <greenmonadcat@gmail.com>
2019-04-06 15:33:50 -0500

    Fix indentation style

--- breadmisc.h
+++ breadmisc.h
@@ -6,16 +6,16 @@
 #include <stdbool.h>
 
 struct registers {
-	uint32_t* register_index[32]; // Convenience.
-	uint32_t r[31]; // General Purpose Registers
-	uint32_t rp; // Instruction Pointer
+   uint32_t* register_index[32]; // Convenience.
+   uint32_t r[31]; // General Purpose Registers
+   uint32_t rp; // Instruction Pointer
 };
 
 struct machine {
-	struct registers regs;
-	uint32_t* mem; // Max 16 GiB
-	size_t mem_size;
-	bool initialized; // If this is false, the struct is unusable.
+   struct registers regs;
+   uint32_t* mem; // Max 16 GiB
+   size_t mem_size;
+   bool initialized; // If this is false, the struct is unusable.
 };
 
 // 2^(5 + (2^4 - 1))
@@ -27,13 +27,13 @@ typedef bool (*predicate)(uint32_t, uint32_t);
 typedef void (*iptr)(struct machine*, uint16_t);
 
 struct instruction {
-	bool high_level; // 1 if for >16 bit processor, 0 otherwise.
-	uint32_t instruction_width; // Bit length of the instruction.
-	uint8_t condition_register_a; // First register for condition check
-	uint8_t condition_register_b; // Second register for condition check
-	predicate condition_operation;
-	iptr fptr;
-	uint16_t instruction_specific_data;
+   bool high_level; // 1 if for >16 bit processor, 0 otherwise.
+   uint32_t instruction_width; // Bit length of the instruction.
+   uint8_t condition_register_a; // First register for condition check
+   uint8_t condition_register_b; // Second register for condition check
+   predicate condition_operation;
+   iptr fptr;
+   uint16_t instruction_specific_data;
 };
 
 // Machine handling utilities
